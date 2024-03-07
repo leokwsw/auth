d9882fec120544169ec8eeb5cd39042999e998d1
Leo Wu <leo.wu.kw@gmail.com>
2024-03-07 11:10:29 +0800

    setup

--- src/main/java/dev/leonardpark/auth/AuthApplication.java
+++ src/main/java/dev/leonardpark/auth/AuthApplication.java
@@ -2,8 +2,13 @@ package dev.leonardpark.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+	UserDetailsServiceAutoConfiguration.class,
+	ReactiveUserDetailsServiceAutoConfiguration.class
+})
 public class AuthApplication {
 
 	public static void main(String[] args) {
